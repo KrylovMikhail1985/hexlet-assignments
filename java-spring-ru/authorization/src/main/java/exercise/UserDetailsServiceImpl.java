1c1abab161fe5b728eace8d23c99e09051ee86e3
@hexlet/cli <support@hexlet.io>
2022-10-13 21:42:54 +0300

    submit java-spring-ru/authorization

---
+++ java-spring-ru/authorization/src/main/java/exercise/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package exercise;
+
+import exercise.model.User;
+import exercise.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        // BEGIN
+        User user = repository.findByEmail(email).orElseThrow(()-> new UserNotFoundException("User not found"));
+        List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(user.getRole().toString()));
+        UserDetails userDetails = org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(authorities)
+                .build();
+        return userDetails;
+        // END
+    }
+}
